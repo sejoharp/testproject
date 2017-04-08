47030196f5c834b73907b8065064d50c0c73c59d
joscha <zippelman@web.de>
2017-04-08 23:07:39 +0200

    initial commit

---
+++ src/main/java/org/harpeng/testproject/Main.java
@@ -0,0 +1,12 @@
+package org.harpeng.testproject;
+
+import org.harpeng.LogHelper;
+
+/**
+ * Created by joscha on 08.04.17.
+ */
+public class Main {
+    public static void main(String [ ] args){
+        LogHelper.info("it works !!!");
+    }
+}
