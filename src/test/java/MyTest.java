de514622625754df78c717af19740691591412b2
joscha <zippelman@web.de>
2017-04-09 08:39:45 +0200

    add first test

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,11 +1,12 @@
 import org.harpeng.LogHelper;
+import org.testng.annotations.Test;
 
 /**
  * Created by joscha on 09.04.17.
  */
-public class Test {
-    @org.testng.annotations.Test
-    public void test(){
-        LogHelper.info("Imported org.harpeng.testlib.");
+public class MyTest {
+    @Test
+    public void test() {
+        LogHelper.info("Import external org.harpeng.testlib works.");
     }
 }
