293fc74c91afb83d96466198928ea896b0005360
joscha <zippelman@web.de>
2017-04-09 08:39:28 +0200

    use testng

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.harpeng.LogHelper;
+
+/**
+ * Created by joscha on 09.04.17.
+ */
+public class Test {
+    @org.testng.annotations.Test
+    public void test(){
+        LogHelper.info("Imported org.harpeng.testlib.");
+    }
+}
